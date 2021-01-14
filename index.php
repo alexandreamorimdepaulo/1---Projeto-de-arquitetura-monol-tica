2d4635eee82c618199f258f4218f678c3b859248
alexandreamorimdepaulo <75735518+alexandreamorimdepaulo@users.noreply.github.com>
2021-01-14 10:09:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Crud</title>
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php require_once 'process.php'; ?>
+
+    <?php
+
+    if (isset($_SESSION['message'])) : ?>
+
+    <div class="alert alert-<?=$_SESSION['msg_type']?>">
+
+    <?php
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+    ?>
+    </div>
+    <?php endif ?>
+    <div class="container">
+    <?php
+    $mysqli = new mysqli('localhost', 'root', 'sua-nova-senha', 'crud') or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+    //pre_r($result);
+    ?>
+    <div class="row justify-content-center">
+    <table class="table">
+    <thead>
+    <tr>
+    <th>Jogos</th>
+    <th>Plataformas</th>
+    <th colspan="2">Alterar</th>
+    </tr>
+    </thead>
+    <?php
+    while($row = $result->fetch_assoc()): ?>
+    <tr>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['location']; ?></td>
+    <td>
+    <a href="index.php?edit=<?php echo $row['id']; ?>"
+    class="btn btn-info">Editar</a>
+    <a href="process.php?delete=<?php echo $row['id']; ?>"
+    class="btn btn-danger">Deletar</a>
+    </td>
+    </tr>
+    <?php endwhile; ?>
+
+    
+    </table>
+
+
+    
+    </div>
+
+
+
+
+<?php
+    function pre_r( $array ) {
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+
+    }
+    ?>
+<div class="row justify-content-center"> 
+<form action="process.php" method="POST">
+<input type="hidden" name="id" value="<?php echo $id; ?>">
+<div class="form-group">
+<label>Jogo</label>
+<input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Nome do jogo">
+</div>
+<div class="form-group">
+<label>Console</label>
+<input type="text" name="location"  class="form-control" value="<?php echo $location; ?>" placeholder="Plataforma">
+</div>
+<div class="form-group">
+<?php 
+if ($update == true):
+?>
+<button type="submit" class="btn-info" name="update">Atualizar</button>
+<?php else: ?>
+<button type="submit" class="btn-primary" name="save">Salvar</button>
+<?php endif; ?>
+</div>
+</form>
+</div>
+</div>
+    
+</body>
+</html>
